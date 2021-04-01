7d3065a41d43c7fea254a4354c084ae6dd354562
lovetrytry <letmetrytry@yeah.net>
2021-04-01 16:46:18 +0800

    添加 RSA 文件获取方法

---
+++ src/Key/RSA.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Lovetrytry\Jichukuangjia\Key;
+
+use Lovetrytry\Jichukuangjia\BaseStatic;
+
+/**
+ * @author letmetrytry
+ * @email letmetrytry@yeah.net
+ */
+class RSA extends BaseStatic
+{
+    public function getPublic()
+    {
+        # code...
+    }
+
+    public function getPublicPath()
+    {
+    }
+}
